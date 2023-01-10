828d7fb3189a4ca39a6bb8ab3e2f27946399dd9b
Leeyonghyeong <tont1990@gmail.com>
2023-01-10 22:37:24 +0900

    메인 페이지 숫자 카운트 작업

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
   },
